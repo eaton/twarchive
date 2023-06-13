6136ba50361ead2d1e9823da1922349f240203c8
Jeff Eaton <jeff@eaton.fyi>
2023-06-13 01:36:22 -0500

    Sorting and shuffling things

--- src/stat.ts
+++ src/stat.ts
@@ -1,6 +1,20 @@
-import { TwitterArchive } from 'twitter-archive-reader';
+import { PartialTweet, TwitterArchive } from 'twitter-archive-reader';
 
 await zipStats();
+
+
+type TweetClusters = {
+  roots: PartialTweet[],
+  threads: Record<string, TweetThread>
+}
+
+type TweetThread = {
+  date: string,
+  tweets: PartialTweet[],
+  favorites?: number,
+  retweets?: number,
+  text?: string,
+}
 
 export async function zipStats(zipfile = 'twitter.zip') {
   const archive = new TwitterArchive(zipfile, {
@@ -19,16 +33,33 @@ export async function zipStats(zipfile = 'twitter.zip') {
 
   archive.releaseZip();
 
-  console.log({
-    handle: archive.user.screen_name,
-    name: archive.user.name,
-    email: archive.user.email_address,
-    bio: archive.user.bio
-  });
+  const data: TweetClusters = {
+    roots: archive.tweets.all.filter(tweet => tweet.user.screen_name === 'eaton' && tweet.in_reply_to_status_id_str === undefined),
+    threads: {}
+  };
 
-  console.log(archive.info.archive)
+  for(const parent of data.roots) {
+    const children = getChildren(archive, [parent]);
+    if (children.length) {
+      data.threads[parent.id_str] = {
+        date: parent.created_at,
+        tweets: [parent, ...children]
+      };
+      console.log(`${parent.created_at} - ${data.threads[parent.id_str].tweets.length} tweet thread`);
+    }
+  }
+}
 
-  console.log(
-    archive.tweets.sortedIterator("desc").next()
-  );
+export function getChildren(archive: TwitterArchive, tweets: PartialTweet[]): PartialTweet[] {
+  const ids = tweets.map(tweet => tweet.id_str);
+  const children = archive.tweets.all.filter(tweet => ids.includes(tweet.in_reply_to_status_id_str ?? ''));
+  if (children.length === 0) return [];
+  return [...children, ...getChildren(archive, children)];
+}
+
+export function getRoot(archive: TwitterArchive, tweet: PartialTweet) {
+  if (tweet.in_reply_to_status_id_str === undefined) return tweet;
+  const parent = archive.tweets.single(tweet.in_reply_to_status_id_str) ?? undefined;
+  if (parent) return getRoot(archive, parent);
+  return undefined;
 }
